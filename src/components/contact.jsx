cedffd53fe547d798399eafb1fb888ceb610ab79
Esteban.much <esteban.muchenvicz@gmail.com>
2023-12-05 21:17:04 -0300

    correccion de errores

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,4 @@
-import { AiFillLinkedin, AiOutlineMail, AiOutlineWhatsApp, AiFillGithub } from 'react-icons/ai';
-import { FaFileDownload } from 'react-icons/fa';
+
 import emailjs from 'emailjs-com';
 import { useState } from 'react';
 import Swal from 'sweetalert2';
@@ -50,100 +49,41 @@ function Contact() {
   
         <form className="w-full" onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block mb-2">Name:</label>
+            <label className="block mb-2 text-gray-500">Nombre</label>
             <input
               type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Name"
+              className="w-full p-2 border border-violet-600 rounded"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Email:</label>
+            <label className="block mb-2 text-gray-500">Correo electrónico</label>
             <input
               type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Mail"
+              className="w-full p-2 border border-violet-600 rounded"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Message:</label>
+            <label className="block mb-2 text-gray-500">Mensaje</label>
             <textarea
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border border-violet-600 rounded"
               rows={5}
-              placeholder="Message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white p-2 rounded mt-4"
+            className="bg-violet-500 text-white p-2 rounded mt-4"
           >
             Enviar
           </button>
         </form>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <h2>Esteban Muchenvicz</h2>
-        <div className="flex flex-col items-center">
-        <ul className="intro-ul flex space-x-4 mt-4">
-        <li>
-                <i><AiOutlineWhatsApp size={40} /></i>
-                <a
-                  href="https://wa.me/543755491171"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="bx bx-md bxl-linkedin-square"></i>
-                </a>
-              </li>
-              <li>
-                <i><AiOutlineMail size={40} /></i>
-                <a
-                  href="mailto:esteban.muchenvicz@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="bx bx-md bxl-linkedin-square"></i>
-                </a>
-              </li>
-              <li>
-                <i><AiFillLinkedin size={40} /></i>
-                <a
-                  href="https://www.linkedin.com/in/esteban-muchenvicz/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="bx bx-md bxl-linkedin-square"></i>
-                </a>
-              </li>
-              <li>
-                <i><AiFillGithub size={40} /></i>
-                <a
-                  href="https://github.com/Estebanmuchenvicz"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="bx bxl-github"></i>
-                </a>
-              </li>
-              <li>
-                <i><FaFileDownload size={40} /></i>
-                <a
-                  href="https://github.com/Estebanmuchenvicz"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="bx bxl-github"></i>
-                </a>
-              </li>
-            </ul>
-        </div>
-      </div>
+      
     </div>
   );
   
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,61 @@
-
+import { AiFillLinkedin, AiOutlineMail, AiOutlineWhatsApp, AiFillGithub } from 'react-icons/ai';
+// import { FaFileDownload } from 'react-icons/fa';
 function Footer() {
   return (
-    <div className="bg-black text-white container-fluid">
-    
+    <div className="mt-8 mb-8">
+          <div className="flex flex-col justify-center items-center">
+        <h2>Esteban Muchenvicz</h2>
+        <div className="flex flex-col items-center">
+        <ul className="intro-ul flex space-x-4 mt-4">
+        <li>
+        <a
+                href="https://wa.me/543755491171"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span><AiOutlineWhatsApp size={40} /></span>
+              </a>
+              </li>
+              <li>
+              <a
+                href="mailto:esteban.muchenvicz@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span><AiOutlineMail size={40} /></span>
+              </a>
+              </li>
+              <li>
+              <a
+                href="https://www.linkedin.com/in/esteban-muchenvicz/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span><AiFillLinkedin size={40} /></span>
+              </a>
+              </li>
+              <li>
+              <a
+                href="https://github.com/Estebanmuchenvicz"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span><AiFillGithub size={40} /></span>
+              </a>
+              </li>
+              {/* <li>
+                <i><FaFileDownload size={40} /></i>
+                <a
+                  href="https://github.com/Estebanmuchenvicz"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="bx bxl-github"></i>
+                </a>
+              </li> */}
+            </ul>
+        </div>
+      </div>
     </div>
   );
 }
