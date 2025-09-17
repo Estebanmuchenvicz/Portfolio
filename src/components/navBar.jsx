f9e2e6bfbf5fe4558efb9919bb95eeb4f2bf1353
Esteban Muchenvicz <esteban.muchenvicz@gmail.com>
2025-09-17 16:08:23 -0300

    correcciones css

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [activeLink, setActiveLink] = useState("");
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   // Detecta el scroll
   useEffect(() => {
